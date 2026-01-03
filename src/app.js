bc327c6f7cb51699e96ffdf0d1e3ab32229eb5c5
Chetan Bhoi <bhoichetan23@gmail.com>
2026-01-03 21:56:00 +0530

    Installing cookie-parser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
-import express from "express";
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const userRoutes = require("./routes/userRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { success: false, message: "Too many requests" },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use("/api", limiter);
 
-export default app;
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
-import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "./config/db.js";
-
-dotenv.config();
+require("dotenv").config();
+const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  await connectDB(); // DB must connect first
-  app.listen(PORT, () => console.log("Server Running on " + PORT));
+const server = async () => {
+  try {
+    await connectDB(); // DB FIRST
+    const server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+    process.on("unhandledRejection", (err) => {
+      console.error("UNHANDLED REJECTION! Shutting down...", err);
+      server.close(() => process.exit(1));
+    });
+  } catch (err) {
+    console.error("DB connection failed ", err);
+    process.exit(1);
+  }
 };
 
-startServer();
+server();
