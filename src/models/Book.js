02940b22a33ab76e6ecec2da9faa7c39fdda66f1
Chetan Bhoi <bhoichetan23@gmail.com>
2026-01-03 21:59:30 +0530

    Updating book model

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,10 +1,69 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-export default mongoose.model(
-  "Book",
-  mongoose.Schema({
-    title: String,
-    author: String,
-    description: String,
-  })
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 200,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    genre: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: [
+        "Fiction",
+        "Non-Fiction",
+        "Science Fiction",
+        "Fantasy",
+        "Mystery",
+        "Thriller",
+        "Romance",
+        "Biography",
+        "History",
+        "Self-Help",
+        "Poetry",
+        "Drama",
+        "Other",
+      ],
+    },
+    publicationYear: {
+      type: Number,
+      required: true,
+      min: 1000,
+      max: new Date().getFullYear(),
+    },
+    ISBN: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: 1000,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
